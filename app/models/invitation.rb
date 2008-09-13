cae66098fb676879ee2cd428c3e13a128352525b
Sandro Turriate <sandro.turriate@gmail.com>
2008-09-13 14:29:30 -0700

    annotated invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20080913211325
+#
+# Table name: invitations
+#
+#  id         :integer(11)     not null, primary key
+#  plan_id    :integer(11)
+#  person_id  :integer(11)
+#  status     :string(255)     default("--- :yes\n")
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Invitation < ActiveRecord::Base
   belongs_to :person
   belongs_to :plan

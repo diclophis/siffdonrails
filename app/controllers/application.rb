790f7c03019015898edf7e07d2469fdd23d5324a
Sandro Turriate <sandro.turriate@gmail.com>
2008-09-13 14:00:45 -0700

    adding basic login ability

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,16 +4,30 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  before_filter :find_or_build_person
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '41b67c2565064bd9c07992127501e5ed'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
   
+  protected
+
   def current_page
     params[:page].to_i < 1 ? 1 : params[:page].to_i
   end
+
+  def find_or_build_person
+    @current_person ||= Person.find_or_initialize_by_id(session[:person_id])
+    if @current_person.new_record?
+      @current_person.save
+      login_as_person
+    end
+  end
+
+  def login_as_person
+    session[:person_id] = @current_person.id
+  end
+
+
 end

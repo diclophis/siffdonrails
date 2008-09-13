384d13544a76756306b5cb2158abed1fc298aede
root <diclophis@gmail.com>
2008-09-13 14:50:33 +0000

    wang chung

--- lib/yql_client.rb
+++ lib/yql_client.rb
@@ -6,27 +6,49 @@ class YqlClient
 
   def initialize
     @config = YAML.load_file("#{RAILS_ROOT}/config/yahoo.yml")['yahoo']
-    @consumer=OAuth::Consumer.new(@config['consumer_key'], @config['shared_secret'], {
-      :site => "http://query.yahooapis.com/",
-    })
-wang = @consumer.create_signed_request(:get, '/v1/yql?q=show%20tables&format=xml')
 
-puts wang.inspect
-
-
-
+@consumer = OAuth::Consumer.new(@config['consumer_key'], @config['shared_secret'], {:http_method => :get})
 @http = Net::HTTP.new('query.yahooapis.com')
 @req = Net::HTTP::Get.new('/v1/yql?q=show%20tables&format=xml')
 @token = OAuth::Token.new('', '')
-#then, configure OAuth for the request:
-@req.oauth!(@http, @consumer, @token)
-
+puts @req.oauth!(@http, @consumer, @token, {:realm => "siffd.com"})
 @response = Net::HTTP.start('query.yahooapis.com', 80) {|http|
-  http.request(@req)
+  puts http.request(@req).inspect
 }
+#puts @consumer.inspect
+#puts @http.inspect
+#puts @req.inspect
+#puts @token.inspect
+#puts @response.inspect
 
-puts @response.inspect
-puts @response.inspect
+#php [0] => Authorization: OAuth oauth_version="1.0",oauth_nonce="8d95c32b157a7404d7e8664e15cb39ad",oauth_timestamp="1221316633",oauth_consumer_key="dj0yJmk9OHZNS1RtZFJERnY2JmQ9WVdrOWQzcHRPR1JVTXpnbWNHbzlNVFV6T0RnNE5EY3dOQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0zMA--",oauth_signature_method="HMAC-SHA1",oauth_signature="A%2BDhNClJErtdZSmnN8jta%2FdHvWg%3D"
+#
+#OAuth realm="", oauth_nonce="pXAwYgVZvwEeKR1cxIfyVwjEfszH4HAQa0n2jgDPk", oauth_signature_method="HMAC-SHA1", oauth_token="", oauth_timestamp="1221316911", oauth_consumer_key="dj0yJmk9OHZNS1RtZFJERnY2JmQ9WVdrOWQzcHRPR1JVTXpnbWNHbzlNVFV6T0RnNE5EY3dOQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0zMA--", oauth_signature="HE9i2EMTfGUTyP3qcH8rXZmyn%2B0%3D", oauth_version="1.0"
+#
+
+
+
+#    puts @config.inspect
+#    @consumer=OAuth::Consumer.new(@config['consumer_key'], @config['shared_secret'], {
+#      :site => "http://query.yahooapis.com/",
+#    })
+#wang = @consumer.create_signed_request(:get, '/v1/yql?q=show%20tables&format=xml')
+#
+#puts wang.inspect
+
+
+
+#@http = Net::HTTP.new('query.yahooapis.com')
+#@req = Net::HTTP::Get.new('/v1/yql?q=show%20tables&format=xml')
+#@token = OAuth::Token.new('', '')
+#@req.oauth!(@http, @consumer, @token)
+#
+#@response = Net::HTTP.start('query.yahooapis.com', 80) {|http|
+#  http.request(@req)
+#}
+#
+#puts @response.inspect
+#puts @response.inspect
 #which sets the Authorization header:
 #@req.oauth!(@http, @consumer, @token, :scheme => 'query_string')
 #setting the scheme to 'query_string' adds the parameters to the query
